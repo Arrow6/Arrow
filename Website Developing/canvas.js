8a202f79be7f1a94f39e85f782f90ff68ae21e18
Arrow6 <48833849+Arrow6@users.noreply.github.com>
2019-03-27 21:45:11 +0200

    Add files via upload

---
+++ Website Developing/canvas.js
@@ -0,0 +1,76 @@
+var canvas = document.querySelector("canvas");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext("2d");
+
+
+
+/*
+var on=false;
+var x = Math.random()*innerWidth;
+var y = Math.random()*innerHeight;
+var dx =(Math.random()-0.4)*10;
+var dy =(Math.random()-0.4)*10;
+var radius =30;
+*/
+
+
+function Circle(x,y,dx,dy,radius){
+this.x=x;
+this.y=y;
+this.dx=dx;
+this.dy=dy;
+this.radius = radius;
+
+this.draw= function(){
+c.beginPath();
+c.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+c.strokeStyle="blue";
+c.stroke();
+
+}
+this.update=function(){
+if(this.x+this.radius>innerWidth || this.x+this.radius<0){
+this.dx= -this.dx;
+}
+if(this.y+this.radius>innerHeight || this.y+this.radius<0){
+this.dy= -this.dy;
+}
+
+this.x+=this.dx;
+this.y+=this.dy;
+this.draw();
+}
+}
+
+
+
+var circleArray=[];
+
+for(a=0;a<250;a++){
+
+var x = Math.random()*innerWidth;
+var y = Math.random()*innerHeight;
+var dx =(Math.random()-0.4)*10;
+var dy =(Math.random()-0.4)*10;
+var radius =30;
+
+circleArray.push(new Circle(x,y,dx,dy,radius));
+
+}
+
+
+function animate(){
+requestAnimationFrame(animate);
+c.clearRect(0,0,innerWidth,innerHeight);
+
+for(i=0;i<circleArray.length;i++){
+circleArray[i].update();
+}
+}
+
+
+animate();
+
